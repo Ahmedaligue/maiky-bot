8a583de778df6e2e80f22db5783814c5b543efa0
Ahmedaligue <158603161+Ahmedaligue@users.noreply.github.com>
2024-08-07 00:22:23 +0100

    Create بيروس.js

---
+++ plugins/بيروس.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `*⎔⋅• ━ ╼╃ ⌬〔🐦‍🔥〕⌬ ╄╾ ━ •⋅⎔
+*~ان شاء الله احمد يتزوج خديجه😉~*`;
+    
+
+    conn.sendFile(m.chat, 'https://telegra.ph/file/d0cbd8f9043ec66b540b3.jpg', 'image.jpg', message, m);
+};
+
+handler.customPrefix = /^(بيروس)$/i;
+handler.command = new RegExp;
+
+export default handler;
