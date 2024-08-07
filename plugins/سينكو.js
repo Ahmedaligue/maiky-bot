efa163e0356768e3384e4b23af7524921dc81d79
Ahmedaligue <158603161+Ahmedaligue@users.noreply.github.com>
2024-08-07 04:58:42 +0100

    Create سينكو.js

---
+++ plugins/سينكو.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `*⎔⋅• ━ ╼╃ ⌬〔🐦‍🔥〕⌬ ╄╾ ━ •⋅⎔
+*الا ان يشرق الامل وتحقق الاماني وينال كل دي حق حق سسكون الحساب عسير على من كان صادقا 👋✌️*`;
+
+
+    conn.sendFile(m.chat, 'https://telegra.ph/file/817e807b798172f5b0399.jpg', 'image.jpg', message, m);
+};
+
+handler.customPrefix = /^(سينكو)$/i;
+handler.command = new RegExp;
+
+export default handler;
